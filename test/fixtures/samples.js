9edd4c292a435bc53a9a1f90210d69b23a04ab78
Pascal Hartig <phartig@rdrei.net>
2013-05-29 10:44:36 +0200

    Updated test case

--- test/fixtures/samples.js
+++ test/fixtures/samples.js
@@ -1,3 +1,53 @@
 if (window.fixtures === undefined) {
   window.fixtures = {};
 }
+
+window.fixtures.fullDoc = {
+  "_links": {
+    "self": {
+      "href": "/"
+    },
+    "curies": [
+      {
+        "name": "ht",
+        "href": "http://haltalk.herokuapp.com/rels/{rel}",
+        "templated": true
+      }
+    ]
+  },
+  "_forms": {
+    "ht:signup": {
+      "href": "/signup",
+      "method": "POST",
+      "schema": {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "description": "Create an account",
+        "title": "signup",
+        "required": [
+          "username",
+          "password"
+        ],
+        "type": "object",
+        "properties": {
+          "username": {
+            "minLength": 1,
+            "type": "string",
+            "description": "The username you want to log in with."
+          },
+          "password": {
+            "type": "string",
+            "description": "The password you want to log in with."
+          },
+          "bio": {
+            "type": "string",
+            "description": "A short description of yourself."
+          },
+          "real_name": {
+            "type": "string",
+            "description": "The name you were born with."
+          }
+        }
+      }
+    }
+  }
+}
--- test/spec/test-resource.js
+++ test/spec/test-resource.js
@@ -2,9 +2,19 @@
 (function () {
   'use strict';
   describe('Resource', function () {
-    it('should still exist', function () {
+    it('should expose a forms attribute', function () {
       var agent = new Hyperagent.Resource('http://example.com/');
-      assert(agent.fetch);
+      agent._load({
+        _links: {
+          self: { href: 'http://example.com' }
+        },
+        _forms: {
+          create: {
+            method: 'POST',
+            href: '/'
+          }
+        }
+      });
     });
   });
 }());
