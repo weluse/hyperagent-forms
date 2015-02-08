78ba89d90496aa6b41ca183c3486460f211b9fd9
Pascal Hartig <phartig@twitter.com>
2015-02-08 14:05:07 +0000

    chore(deps): upgrade test dependencies

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -1,70 +1,61 @@
-// Karma configuration
+module.exports = function(config) {
+  config.set({
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
-// list of files / patterns to load in the browser
-files = [
-  MOCHA,
-  MOCHA_ADAPTER,
-  'components/chai/chai.js',
-  'components/uri.js/src/URI.js',
-  'components/uri.js/src/URITemplate.js',
-  'components/q/q.js',
-  'components/hyperagent/dist/hyperagent.js',
-  'components/tv4/tv4.js',
-  'dist/hyperagent-forms.js',
-  'test/setup.js',
-  'test/lib/utils.js',
-  'test/fixtures/**/*.js',
-  'test/spec/**/*.js'
-];
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha'],
 
-// list of files to exclude
-exclude = [];
+    // list of files / patterns to load in the browser
+    files: [
+      'bower_components/chai/chai.js',
+      'bower_components/uri.js/src/URI.js',
+      'bower_components/uri.js/src/URITemplate.js',
+      'bower_components/q/q.js',
+      'bower_components/hyperagent/dist/hyperagent.js',
+      'bower_components/tv4/tv4.js',
+      'dist/hyperagent-forms.js',
+      'test/setup.js',
+      'test/lib/utils.js',
+      'test/fixtures/**/*.js',
+      'test/spec/**/*.js'
+    ],
 
-preprocessors = {
-  'dist/hyperagent-forms.js': 'coverage'
+    // list of files to exclude
+    exclude: [
+    ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS2'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
+  });
 };
-
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['coverage', 'progress'];
-
-coverageReporter = {
-  type: 'lcov',
-  dir: 'coverage/'
-}
-
-// web server port
-port = 8080;
-
-// cli runner port
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
